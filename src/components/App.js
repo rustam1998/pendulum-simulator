da7b84adcb09b22df3ed5f88b049364d5cfe45b8
rustam1998 <rustamsahatov50@gmail.com>
2019-03-11 00:55:18 +0200

    done with functionality

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ const t = T / 350;
 //     //коэф. затухания
 // const b = r/(2*m)
 //console.log(b)
+
 let id;
 
 export default class pendMain extends Component {
@@ -23,31 +24,76 @@ export default class pendMain extends Component {
       timeElapsed: 0,
       environment: '',
       b: 0,
-      class: ''
-  }
-    // componentDidMount(){
-  //   webkitRequestAnimationFrame(this.PendulumSim)
-  // }
- 
-  air = () =>{
-    this.setState({environment: 'air'})
-    this.setState({b: 0.004765508990216238})
-    webkitRequestAnimationFrame(this.PendulumSim)
+      class: '',
+      showBtns: true
   }
   
-  water = () =>{
-    this.setState({environment: 'water'})
-    this.setState({b: 0.02})
-    webkitRequestAnimationFrame(this.PendulumSim)
-    this.setState({class: 'waterDiv'})
+  //Рисую маятник до начала движения || Drawing pendulum before rotating
+  componentDidMount(){
+    if(this.state.environment !== 'air' || 'water'){
+      const context = this.refs.canvas.getContext("2d");
+      
+      context.fillStyle = "grey";
+      context.beginPath();
+      context.rect(320, 100, 4, 275);
+      context.fill();
+      context.stroke();
+ 
+      context.beginPath();
+      context.arc(322, 340, 20, 0, Math.PI*2, false);
+      context.fill();
+      context.stroke();
+    
+      context.beginPath();
+      context.moveTo(322,410);
+      context.lineTo(319.5,375);
+      context.lineTo(324.3,375);
+      context.closePath();
+      context.stroke();
+    }
+  }
+ 
+  //Обновить страницу || reload page  
+  reloadPage = () => {
+    window.location.reload();
   }
 
-  toggle = () => {
+  //обработчик кнопки "в воздухе" || 'on air' button's handler
+  air = () =>{
+
+    this.setState({environment: 'air'})
+    // Изменяю сопротивление среды || changing resistance of the environment
+    this.setState({b: 0.004765508990216238})
+    // начало отсчета таймера || starting timer
+    this.setState({isRunning: !this.state.isRunning})
     this.setState({isRunning: !this.state.isRunning}, () => {
-      this.state.isRunning ? this.startTimer() : clearInterval(this.timer)
+      this.state.isRunning ? this.startTimer() : ''
     });
-     
-    this.state.isRunning ? cancelAnimationFrame(id) :''
+    this.setState({showBtns: false})
+    //Начало анимации|| Animation's starting
+    webkitRequestAnimationFrame(this.PendulumSim)
+  }
+
+  //обработчик кнопки "в воде" || 'in water' button's handler
+  water = () =>{
+    this.setState({environment: 'water'})
+    // Изменяю сопротивление среды || changing resistance of the environment
+    this.setState({b: 0.02})
+    //добавляю воду || adding water
+    this.setState({class: 'waterDiv'})
+    // начало отсчета таймера || starting timer
+    this.setState({isRunning: !this.state.isRunning}, () => {
+      this.state.isRunning ? this.startTimer() : ''
+    });
+    this.setState({showBtns: false})
+    //Начало анимации|| Animation's starting
+    webkitRequestAnimationFrame(this.PendulumSim)
+  }
+
+  //остановка анимации и таймера || Stopping timer and animation
+  toggle = () => {
+    clearInterval(this.timer)
+    this.state.isRunning ? cancelAnimationFrame(id) : ''
   }
 
   startTimer = () => {
@@ -60,20 +106,26 @@ export default class pendMain extends Component {
     this.startTime = Date.now();
   }
  
+  //отрисовка движущегося маятника || drawing a moving pendulum
   PendulumSim = () => {
     const canvas = this.refs.canvas
-    const context = this.refs.canvas.getContext("2d");
-  
+    const context = canvas.getContext("2d");
+    //удаление предыдущего кадра|| removing previos frame
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
     context.fillStyle = "rgba(255,255,255,0.91)";
     context.fillRect(0, 0, canvas.width, canvas.height);
  
     context.fillStyle = "grey";
+    //Наложение фигур друг на друга|| Overlay figures on each other
     context.globalCompositeOperation = "source-over";
  
     context.save();
       context.translate(320, 100)
+      //изменение угла вращения || changing rotatiion angle
       context.rotate(angle);
- 
+
+      //Рисую фигуры || Drawing figures
       context.beginPath();
       context.rect(0, 0, 4, 275);
       context.fill();
@@ -91,8 +143,11 @@ export default class pendMain extends Component {
       context.closePath();
       context.stroke();
     context.restore();
+      //Рассчет ускорения маятника || calculate acceleration of the pendulum
      let acceleration =  -Math.sin(angle) - this.state.b * V;
+      //Рассчет скорости вращения || calculate velocity of the pendulum
       V += acceleration * t;
+      //Рассчет угла вращения || calculate rotation angle of the pendulum
       angle += V * t;
       id = webkitRequestAnimationFrame(this.PendulumSim) 
   }
@@ -107,8 +162,15 @@ export default class pendMain extends Component {
     
     return (
       <div>
-        <button onClick={this.air}>В воздухе</button>
-        <button onClick={this.water}>В воде</button>
+        {
+          this.state.showBtns ?
+          <div>
+          <button onClick={this.air}>В воздухе</button>
+            <button onClick={this.water}>В воде</button>
+          </div> 
+          : 
+          <button onClick={this.reloadPage}>Обнулить</button>
+        }
         <canvas id="canvas" width="700" height="420" ref="canvas"></canvas>
         <div className={this.state.class}></div>
         <div className="chevron">
@@ -126,13 +188,14 @@ export default class pendMain extends Component {
           <p>|</p>
           <p>|</p>
         </div>
+
         <div className="timer">
             <span>{min}:</span>
             <span>{sec}.</span>
             <span>{msec}</span>
             </div>
             <button onClick={this.toggle} className='startBtn'>
-              {isRunning ? 'Stop' : 'Start'}
+              Stop
             </button>
        </div>
     )
